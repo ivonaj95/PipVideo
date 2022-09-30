4d2db74755b5d590c8219c2f313fa6a4b25f99b2
Ivona Jurosevic <ivona.jurosevic@rt-rk.com>
2022-09-30 12:49:13 +0200

    Back to full screen when start app again

--- app/src/main/java/com/example/pipvideo/MainActivity.kt
+++ app/src/main/java/com/example/pipvideo/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.example.pipvideo
 
 import android.app.PictureInPictureParams
+import android.content.Intent
+import android.content.res.Configuration
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.media3.common.MediaItem
 import androidx.media3.common.MimeTypes
@@ -15,6 +19,7 @@ class MainActivity : AppCompatActivity() {
 
     private var exoPlayer: ExoPlayer? = null
     private lateinit var playerView: PlayerView
+    private lateinit var textView: TextView
 
     private var playWhenReady = true
     private var currentItem = 0
@@ -26,16 +31,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         playerView = findViewById(R.id.video_view)
+        textView = findViewById(R.id.text_view)
+        Log.d("IVONA", "onCreate")
     }
 
     override fun onStart() {
         super.onStart()
+        Log.d("IVONA", "onStart")
         initializePlayer()
     }
 
     public override fun onStop() {
         super.onStop()
-      //  releasePlayer()
+        Log.d("IVONA", "onStop")
+        releasePlayer()
     }
 
     private fun releasePlayer() {
@@ -51,24 +60,28 @@ class MainActivity : AppCompatActivity() {
 
 
     private fun initializePlayer() {
-        exoPlayer = ExoPlayer.Builder(this)
-            .build()
-            .also { exoPlayer ->
-                playerView.player = exoPlayer
+        Log.d("IVONA", "initializePlayer")
+        if (exoPlayer == null) {
+            Log.d("IVONA", "initializePlayer was null")
+            exoPlayer = ExoPlayer.Builder(this)
+                .build()
+                .also { exoPlayer ->
+                    playerView.player = exoPlayer
 
-                val mediaItem = MediaItem.Builder()
-                    .setUri(getString(R.string.media_url_dash))
-                    .setMimeType(MimeTypes.APPLICATION_MPD)
-                    .build()
+                    val mediaItem = MediaItem.Builder()
+                        .setUri(getString(R.string.media_url_dash))
+                        .setMimeType(MimeTypes.APPLICATION_MPD)
+                        .build()
 
 //                val mediaItem = MediaItem.fromUri(getString(R.string.media_url_mp4)) // mp4 or mp3
-                exoPlayer.setMediaItem(mediaItem)
-                exoPlayer.playWhenReady = playWhenReady
-                exoPlayer.seekTo(currentItem, playbackPosition)
-                exoPlayer.addListener(playbackListener)
-                exoPlayer.prepare()
+                    exoPlayer.setMediaItem(mediaItem)
+                    exoPlayer.playWhenReady = playWhenReady
+                    exoPlayer.seekTo(currentItem, playbackPosition)
+                    exoPlayer.addListener(playbackListener)
+                    exoPlayer.prepare()
 
-            }
+                }
+        }
 
     }
 
@@ -89,5 +102,31 @@ class MainActivity : AppCompatActivity() {
         super.onUserLeaveHint()
         Log.d("IVONA", "onUserLeaveHint")
         enterPictureInPictureMode(PictureInPictureParams.Builder().build())
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("IVONA", "onPause")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("IVONA", "onResume")
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        Log.d("IVONA", "onNewIntent " + intent.toString())
+    }
+
+    override fun onPictureInPictureModeChanged(
+        isInPictureInPictureMode: Boolean, newConfig: Configuration
+    ) {
+        super.onPictureInPictureModeChanged(isInPictureInPictureMode, newConfig)
+        if (isInPictureInPictureMode) {
+            textView.visibility = View.GONE
+        } else {
+            textView.visibility = View.VISIBLE
+        }
     }
 }
