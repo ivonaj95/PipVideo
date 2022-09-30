c57222552366d7ebe4f0e5011bd3f861b21ae3de
Ivona Jurosevic <ivona.jurosevic@rt-rk.com>
2022-09-30 09:57:06 +0200

    Add video player and pip

--- app/src/main/java/com/example/pipvideo/MainActivity.kt
+++ app/src/main/java/com/example/pipvideo/MainActivity.kt
@@ -1,11 +1,93 @@
 package com.example.pipvideo
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.PictureInPictureParams
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.media3.common.MediaItem
+import androidx.media3.common.MimeTypes
+import androidx.media3.common.Player
+import androidx.media3.common.util.Util
+import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.ui.PlayerView
 
 class MainActivity : AppCompatActivity() {
+
+    private var exoPlayer: ExoPlayer? = null
+    private lateinit var playerView: PlayerView
+
+    private var playWhenReady = true
+    private var currentItem = 0
+    private var playbackPosition = 0L
+
+    private val playbackListener = playbackStateListener()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        playerView = findViewById(R.id.video_view)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        initializePlayer()
+    }
+
+    public override fun onStop() {
+        super.onStop()
+      //  releasePlayer()
+    }
+
+    private fun releasePlayer() {
+        exoPlayer?.let { exoPlayer ->
+            playbackPosition = exoPlayer.currentPosition
+            currentItem = exoPlayer.currentMediaItemIndex
+            playWhenReady = exoPlayer.playWhenReady
+            exoPlayer.removeListener(playbackListener)
+            exoPlayer.release()
+        }
+        exoPlayer = null
+    }
+
+
+    private fun initializePlayer() {
+        exoPlayer = ExoPlayer.Builder(this)
+            .build()
+            .also { exoPlayer ->
+                playerView.player = exoPlayer
+
+                val mediaItem = MediaItem.Builder()
+                    .setUri(getString(R.string.media_url_dash))
+                    .setMimeType(MimeTypes.APPLICATION_MPD)
+                    .build()
+
+//                val mediaItem = MediaItem.fromUri(getString(R.string.media_url_mp4)) // mp4 or mp3
+                exoPlayer.setMediaItem(mediaItem)
+                exoPlayer.playWhenReady = playWhenReady
+                exoPlayer.seekTo(currentItem, playbackPosition)
+                exoPlayer.addListener(playbackListener)
+                exoPlayer.prepare()
+
+            }
+
+    }
+
+    private fun playbackStateListener() = object : Player.Listener {
+        override fun onPlaybackStateChanged(playbackState: Int) {
+            val stateString: String = when (playbackState) {
+                ExoPlayer.STATE_IDLE -> "ExoPlayer.STATE_IDLE      -"
+                ExoPlayer.STATE_BUFFERING -> "ExoPlayer.STATE_BUFFERING -"
+                ExoPlayer.STATE_READY -> "ExoPlayer.STATE_READY     -"
+                ExoPlayer.STATE_ENDED -> "ExoPlayer.STATE_ENDED     -"
+                else -> "UNKNOWN_STATE             -"
+            }
+            Log.d("IVONA", "changed state to $stateString")
+        }
+    }
+
+    override fun onUserLeaveHint() {
+        super.onUserLeaveHint()
+        Log.d("IVONA", "onUserLeaveHint")
+        enterPictureInPictureMode(PictureInPictureParams.Builder().build())
     }
 }
